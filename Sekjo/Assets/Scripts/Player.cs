e5e95918bbbe2cd05e3263e113b383644ccd6834
Baldacchino97 <33151935+Baldacchino97@users.noreply.github.com>
2021-01-21 13:26:06 +0100

    Start of Game
    Created Player
    Created Player Jumping
    Created Player Slamming

---
+++ Sekjo/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    enum PlayerState
+    {
+        Standing, Jumping, Falling
+    }
+
+    PlayerState CurrentState = PlayerState.Falling;
+
+    Rigidbody2D rb;
+    //BoxCollider2D bc2;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //bc2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (CurrentState == PlayerState.Standing)
+            {
+                Jump();
+            }
+            else if (CurrentState == PlayerState.Jumping)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+    }
+
+    void Jump()
+    {
+        Debug.Log("jump");
+        CurrentState = PlayerState.Jumping;
+        rb.velocity = new Vector2(0, 10);
+    }
+
+    IEnumerator Shoot()
+    {
+        Debug.Log("shoot");
+
+        CurrentState = PlayerState.Falling;
+        
+        rb.isKinematic = true;
+        rb.velocity = new Vector2(0,0);
+
+        yield return new WaitForSeconds(0.5f);
+
+        rb.isKinematic = false;
+        rb.velocity = new Vector2(0,-30);
+
+        yield break;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        CurrentState = PlayerState.Standing;
+    }
+}
