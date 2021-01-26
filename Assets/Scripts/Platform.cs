26116bcd53814d0e0a6a46dff9cfbb185b4d3ba5
Baldacchino97 <33151935+Baldacchino97@users.noreply.github.com>
2021-01-26 18:28:59 +0100

    2nd Commit - Platform Movement
    Player Jumps through Platforms
    Platform Velocity Movement
    Player Bounces Off walls
    Respawn when player dies

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    float velocity = 1;
+    float distance = 3;
+    float angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = new Vector2(Mathf.Sin(angle) * distance, transform.position.y);
+        angle += velocity / 100f;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -10,16 +11,22 @@ public class Player : MonoBehaviour
         Standing, Jumping, Falling
     }
 
+    public Transform playerParentTransform;
+
     PlayerState CurrentState = PlayerState.Falling;
 
     Rigidbody2D rb;
-    //BoxCollider2D bc2;
+    BoxCollider2D bc2D;
     
     // Start is called before the first frame update
+    float previousPosXofParent;
+
+    bool isDead = false;
+    
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        //bc2D = GetComponent<BoxCollider2D>();
+        bc2D = GetComponent<BoxCollider2D>();
     }
 
     void Start()
@@ -30,6 +37,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         GetInput();
+        BounceAtWall();
+        GetPreviousPositionOfParent();
+        DeadCheck();
     }
 
     void GetInput()
@@ -47,17 +57,28 @@ public class Player : MonoBehaviour
         }
     }
 
+    void GetPreviousPositionOfParent()
+    {
+        previousPosXofParent = transform.parent.transform.position.x;
+    }
+
     void Jump()
     {
-        Debug.Log("jump");
+        bc2D.enabled = false;
         CurrentState = PlayerState.Jumping;
-        rb.velocity = new Vector2(0, 10);
+
+        rb.velocity = new Vector2(ParentVelocity(), 10);
+
+        transform.SetParent(playerParentTransform);
+    }
+
+    float ParentVelocity()
+    {
+        return (transform.parent.transform.position.x - previousPosXofParent) / Time.deltaTime;
     }
 
     IEnumerator Shoot()
     {
-        Debug.Log("shoot");
-
         CurrentState = PlayerState.Falling;
         
         rb.isKinematic = true;
@@ -65,14 +86,40 @@ public class Player : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
+        bc2D.enabled = true;
         rb.isKinematic = false;
         rb.velocity = new Vector2(0,-30);
 
         yield break;
     }
 
+    void BounceAtWall()
+    {
+        if (rb.position.x < - 2.5f)
+        {
+            rb.position = new Vector2(-2.8f, rb.position.y);
+            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+        }
+
+        if (rb.position.x > 2.8f)
+        {
+            rb.position = new Vector2(2.8f, rb.position.y);
+            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+        }
+    }
+
+    void DeadCheck()
+    {
+        if (isDead == false && Camera.main.transform.position.y - transform.position.y > 10)
+        {
+            isDead = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         CurrentState = PlayerState.Standing;
+        transform.SetParent(other.gameObject.transform);
     }
 }
